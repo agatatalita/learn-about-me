e7bc86bfe5ea4eecfdd52549ae164336c31bf50f
Agata Andrzejewska <agatamonikaandrzejewska@gmail.com>
2016-12-15 23:45:37 +0100

    add database functions, add ejs views and routing

--- routes/users.js
+++ routes/users.js
@@ -19,4 +19,85 @@ router.get('/', function(req, res, next) {
   	);
 });
 
+/* GET New User page */
+router.get('/newuser', function (req, res) {
+  res.render('newuser', {title: 'Add New User' });
+  //TODO error
+});
+
+/* POST to Add User */
+router.post('/adduser', function (req,res) {
+  //get form values. 
+  const user = {
+    "username": req.body.username,
+    "email": req.body.useremail
+  };
+  service.addUser(user)
+    .then(
+      function (result) { console.log(result); res.redirect("/users");},
+      function (err) { console.error('failed adding user', err);
+      //TODO http code?
+      res.send("There was a problem adding the information to the database");
+    }
+  );
+});
+
+/* GET User by key. */
+router.get('/:key', function (req, res) {
+  //get key value from url
+  const userkey = req.params.key;
+  console.log("cz dziku ", userkey);
+  service.getUserByKey(userkey)
+    .then( 
+      function (list) {
+        console.log(list);
+        res.render('userinfo', {"user": list[0] });
+      },
+      function (err) {
+        console.error('failed getting user', err);
+        //TODO http code?
+        res.send("There was a problem getting the information from the database")
+      }
+
+    )
+});
+// TODO
+/* REMOVE USER */
+
+router.get('/:key/update', function (req, res) {
+  const userkey = req.params.key;
+  console.log("cz dziku ", userkey);
+  service.getUserByKey(userkey)
+    .then( 
+      function (list) {
+        console.log(list);
+        res.render('userupdate', {"user": list[0]});
+      },
+      function (err) {
+        res.send("Failed to update user.");
+        //TODO http code?
+      }
+
+    )
+
+});
+router.post('/:key/update', function (req, res) {
+  const user = {
+    "key": req.params.key,
+    "username": req.body.username,
+    "email": req.body.useremail
+  }
+  service.updateUser(user).then(
+    function (list) {
+      console.log(list); res.redirect("/users");
+    },
+    function (err) {
+      res.send("Failed to update user.");
+    }
+  )
+});
+
+/* POST UPDATE USER */
+/* GET UPDATE USER */
+
 module.exports = router;
--- services/dataServices.js
+++ services/dataServices.js
@@ -1,13 +1,57 @@
 'use strict';
 const Database = require('arangojs');
 const db = new Database({url:'http://127.0.0.1:8529'});
+//TODO user authorisation
+db.useBasicAuth('root');
+db.useDatabase('lamDB');
 module.exports = {
 
 	getAllUsers : function() {
-		db.useBasicAuth('root');
-		db.useDatabase('lamDB');
 		return db.query('FOR x IN Users RETURN x')
 				.then(function (cursor) { return cursor.all();});
+	},
+	getUserByKey : function(userKey) {
+	    const bindVars = {'userKey': userKey};
+	    return db.query('FOR x IN Users FILTER x._key == @userKey RETURN x', bindVars)
+				.then(
+					function (cursor) { 
+					  return cursor.all();
+					},
+					//TODO handling errors
+					function (err) { 
+						console.error('Problem while getting user by Id', err);
+					}
+				);
+	},
+	addUser : function(user) {
+		const collection = db.collection('Users');
+		return collection.save(user);
+		//TODO handling errors ?
+	},
+	updateUser : function(user) {
+		const bindVars = {'key': user.key, 'username' : user.username, 'email' :user.email };
+		return db.query('FOR x IN Users FILTER x._key == @key UPDATE x WITH { username: @username, email: @email} IN Users', bindVars)
+				.then(
+					function (cursor) {
+						return cursor.all();
+					},
+					function (err) { 
+						console.error('Failed while updating Users Database', err);
+					}
+				);
+	},
+	removeUser : function(userKey) {
+		const bindVars = {'userKey': key};
+		return db.query('FOR x IN Users FILTER x._key == @key REMOVE x IN Users LET removed = OLD RETURN removed', bindVars)
+				.then(
+					function(cursor) {
+						return cursor.all();
+					},
+					function(err) {
+						console.error('Failed while removing User');
+					}
+				);
 	}
+
 
 }
