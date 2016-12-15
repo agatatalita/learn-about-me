44406527c0005983b9c86c3c8a1ea4bd0ea4dad1
Agata Andrzejewska <agatamonikaandrzejewska@gmail.com>
2016-12-15 23:59:00 +0100

    tidy up code

--- initialisation/initdb.js
+++ initialisation/initdb.js
@@ -1,28 +1,12 @@
 'use strict';
 const Database = require('arangojs').Database;
 const db = new Database();
-let collection
 db.useBasicAuth('root');
 
-yield db.createDatabase('lamDBtest334');
-db.useDatabase('lamDBtest334');
-collection = db.collection('Users');
+yield db.createDatabase('lamDB');
+db.useDatabase('lamDB');
+const collection = db.collection('Users');
 collection.create();
-
-/*
-db.createDatabase('lamDB').then(
-  () => console.log('Database created'),
-  err => console.error('Failed to create database:', err)
-).then(
-  () => db.useDatabase('lamDB'),
-  err => console.error('failed to bind database', err)
-).then(
-  () => collection = db.collection('Users'),
-  err => console.error('failed to create handle', err)
-).then(
-  () => collection.create(),
-  err => console.error('failed to create collection', err)
-);*/
 
 
 
--- routes/users.js
+++ routes/users.js
@@ -1,28 +1,28 @@
-let express = require('express');
-let router = express.Router();
-let service = require('../services/dataServices.js')
+const express = require('express');
+const router = express.Router();
+const service = require('../services/dataServices.js')
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   console.log("Geting Users list");
   // geting users list from data service
   service.getAllUsers().then(
-  	function (list) {
+  	(list) => {
   	  console.log(list);
   	  //render userlist view with list of users
   	  res.render('userlist', { "userlist": list});
   	},
-  	function (err) {
+  	(err) => {
   	  console.error("Getting all users failed: ", err);
   	  res.send("There was a problem getting users list from database")
   	}
-  	);
+  );
 });
 
 /* GET New User page */
 router.get('/newuser', function (req, res) {
   res.render('newuser', {title: 'Add New User' });
-  //TODO error
+  //TODO error?
 });
 
 /* POST to Add User */
@@ -32,10 +32,9 @@ router.post('/adduser', function (req,res) {
     "username": req.body.username,
     "email": req.body.useremail
   };
-  service.addUser(user)
-    .then(
-      function (result) { console.log(result); res.redirect("/users");},
-      function (err) { console.error('failed adding user', err);
+  service.addUser(user).then(
+    (result) => {console.log(result); res.redirect("/users");},
+    (err) => { console.error('failed adding user', err);
       //TODO http code?
       res.send("There was a problem adding the information to the database");
     }
@@ -44,43 +43,42 @@ router.post('/adduser', function (req,res) {
 
 /* GET User by key. */
 router.get('/:key', function (req, res) {
-  //get key value from url
   const userkey = req.params.key;
   console.log("cz dziku ", userkey);
-  service.getUserByKey(userkey)
-    .then( 
-      function (list) {
+  service.getUserByKey(userkey).then( 
+    (list) => {
         console.log(list);
         res.render('userinfo', {"user": list[0] });
-      },
-      function (err) {
+    },
+    (err) => {
         console.error('failed getting user', err);
         //TODO http code?
         res.send("There was a problem getting the information from the database")
-      }
-
-    )
+    }
+  )
 });
+
 // TODO
 /* REMOVE USER */
 
+/* GET UPDATE USER */
 router.get('/:key/update', function (req, res) {
   const userkey = req.params.key;
-  console.log("cz dziku ", userkey);
-  service.getUserByKey(userkey)
-    .then( 
-      function (list) {
-        console.log(list);
-        res.render('userupdate', {"user": list[0]});
-      },
-      function (err) {
-        res.send("Failed to update user.");
-        //TODO http code?
-      }
+  service.getUserByKey(userkey).then( 
+    (list) => {
+      console.log(list);
+      res.render('userupdate', {"user": list[0]});
+    },
+    (err) => {
+      res.send("Failed to update user.");
+      //TODO http code?
+    }
 
-    )
+  )
 
 });
+
+/* POST UPDATE USER */
 router.post('/:key/update', function (req, res) {
   const user = {
     "key": req.params.key,
@@ -88,16 +86,9 @@ router.post('/:key/update', function (req, res) {
     "email": req.body.useremail
   }
   service.updateUser(user).then(
-    function (list) {
-      console.log(list); res.redirect("/users");
-    },
-    function (err) {
-      res.send("Failed to update user.");
-    }
+    (list) => {console.log(list); res.redirect("/users");},
+    (err) => res.send("Failed to update user.")
   )
 });
-
-/* POST UPDATE USER */
-/* GET UPDATE USER */
 
 module.exports = router;
